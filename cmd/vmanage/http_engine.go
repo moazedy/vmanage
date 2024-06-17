473c3c46d79e4d971b537ec0f62e90b6202a1d4f
moazedy <moazedy@gmail.com>
2024-06-17 21:29:27 +0330

    updating api routes and adding new apis for oauth

--- cmd/vmanage/http_engine.go
+++ cmd/vmanage/http_engine.go
@@ -4,13 +4,16 @@ import "github.com/gin-gonic/gin"
 
 func run(address string, items serverItems) {
 	engine := gin.Default()
+	engine.GET("/auth/google/login", items.oauthPresentation.Login)
+	engine.GET("/auth/google/callback", items.oauthPresentation.Callback)
 
-	// APIs
-	engine.POST("/vehicles/vehicle", items.vehiclePresentation.Create)
-	engine.PUT("/vehicles/vehicle", items.vehiclePresentation.Update)
-	engine.DELETE("/vehicles/vehicle/:id", items.vehiclePresentation.Delete)
-	engine.GET("/vehicles/vehicle/:id", items.vehiclePresentation.GetByID)
-	engine.GET("/vehicles/vehicle/title/:title", items.vehiclePresentation.GetByTitle)
+	privateApisGroup := engine.Group("private")
+	privateApisGroup.Use(items.oauthPresentation.CheckLogin)
+	privateApisGroup.POST("/vehicles/vehicle", items.vehiclePresentation.Create)
+	privateApisGroup.PUT("/vehicles/vehicle", items.vehiclePresentation.Update)
+	privateApisGroup.DELETE("/vehicles/vehicle/:id", items.vehiclePresentation.Delete)
+	privateApisGroup.GET("/vehicles/vehicle/:id", items.vehiclePresentation.GetByID)
+	privateApisGroup.GET("/vehicles/vehicle/title/:title", items.vehiclePresentation.GetByTitle)
 
 	// running the http engine
 	err := engine.Run(address)
